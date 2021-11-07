be197c35a82d101a414d5c2e59df4a46cce4f527
ganfanrenShiJiali <91459872+ganfanrenShiJiali@users.noreply.github.com>
2021-11-07 16:17:49 +0800

    Create Random.c

---
+++ week6/ch03/Random.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define A 48271L
+#define M 2147483647L
+#define Q (M/A)
+#define R (M%A)
+
+double Random(void)
+{
+	long TmpSeed;
+	TmpSeed = A * (Seed%Q) - R * (Seed / Q);
+	if (TmpSeed >= 0)
+		Seed = TmpSeed;
+	else
+		Seed = TmpSeed + M;
+	return (double)Seed / M;
+}
+void Initialize(unsigned long InitVal)
+{
+	Seed = InitVal;
+}
