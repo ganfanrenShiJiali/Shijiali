8fa217478426bfb5fe4b9bddbb27a95d399b51e6
ganfanrenShiJiali <91459872+ganfanrenShiJiali@users.noreply.github.com>
2022-01-09 21:59:01 +0800

    Create reverseList.cpp

---
+++ leetcode/reverseList/reverseList.cpp
@@ -0,0 +1,25 @@
+//迭代
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
+
+//递归
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return p;
+    }
